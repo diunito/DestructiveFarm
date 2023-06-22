19d7768f88da607261727f9d1c748aedd1371497
Alessandro <alessandromilani2000@gmail.com>
2023-06-22 11:34:42 +0200

    Dockerizer

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
+import os
+
 CONFIG = {
     # Don't forget to remove the old database (flags.sqlite) before each competition.
 
@@ -5,14 +7,23 @@ CONFIG = {
     # fetch FLAG_FORMAT from sploits' stdout.
     'TEAMS': {'Team #{}'.format(i): '10.0.0.{}'.format(i)
               for i in range(1, 29 + 1)},
-    'FLAG_FORMAT': r'[A-Z0-9]{31}=',
+    'FLAG_FORMAT': os.environ.get(r'FLAG_FORMAT', r'[A-Z0-9]{31}='),
 
     # This configures how and where to submit flags.
     # The protocol must be a module in protocols/ directory.
 
-    'SYSTEM_PROTOCOL': 'ructf_tcp',
-    'SYSTEM_HOST': '127.0.0.1',
-    'SYSTEM_PORT': 31337,
+    'SYSTEM_PROTOCOL': os.environ.get('SYSTEM_PROTOCOL', 'volgactf'),
+    'SYSTEM_HOST': os.environ.get('SYSTEM_HOST', '10.10.10.10'),
+    'SYSTEM_PORT': int(os.environ.get('SYSTEM_PORT', 31337)),
+    'SYSTEM_URL': os.environ.get('SYSTEM_URL', 'http://monitor.ructfe.org/flags'),
+    'SYSTEM_TOKEN': os.environ.get('SYSTEM_TOKEN', 'your_secret_token'),
+    
+    
+    'TEAM_TOKEN': os.environ.get('TEAM_TOKEN', 'your_secret_token'),
+    
+    #'SYSTEM_PROTOCOL': 'ructf_tcp',
+    #'SYSTEM_HOST': '127.0.0.1',
+    #'SYSTEM_PORT': 31337,
 
     # 'SYSTEM_PROTOCOL': 'ructf_http',
     # 'SYSTEM_URL': 'http://monitor.ructfe.org/flags',
@@ -29,13 +40,13 @@ CONFIG = {
     # The server will submit not more than SUBMIT_FLAG_LIMIT flags
     # every SUBMIT_PERIOD seconds. Flags received more than
     # FLAG_LIFETIME seconds ago will be skipped.
-    'SUBMIT_FLAG_LIMIT': 50,
-    'SUBMIT_PERIOD': 5,
-    'FLAG_LIFETIME': 5 * 60,
+    'SUBMIT_FLAG_LIMIT': float(os.environ.get('SUBMIT_FLAG_LIMIT', 100)),
+    'SUBMIT_PERIOD': float(os.environ.get('SUBMIT_PERIOD', 5)),    
+    'FLAG_LIFETIME': float(os.environ.get('FLAG_LIFETIME', 60)),
 
     # Password for the web interface. You can use it with any login.
     # This value will be excluded from the config before sending it to farm clients.
-    'SERVER_PASSWORD': '1234',
+    'SERVER_PASSWORD': os.environ.get('SERVER_PASSWORD', '1234'),
 
     # Use authorization for API requests
     'ENABLE_API_AUTH': False,
