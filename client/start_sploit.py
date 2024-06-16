8f420dc7f67668cb6b7978a70a2948658705ee92
koraynilay <koray.fra@gmail.com>
2024-06-16 17:34:24 +0200

    Make start_sploit.py catch SIGINT in addition to KeyboardInterrupt

--- client/start_sploit.py
+++ client/start_sploit.py
@@ -16,6 +16,7 @@ import stat
 import subprocess
 import time
 import threading
+import signal # to catch SIGINT
 from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
 from math import ceil
@@ -195,7 +196,6 @@ if os_windows:
     # By default, Ctrl+C does not work on Windows if we spawn subprocesses.
     # Here we fix that using WinApi. See https://stackoverflow.com/a/43095532
 
-    import signal
     import ctypes
     from ctypes import wintypes
 
@@ -570,11 +570,14 @@ def shutdown():
         for proc in instance_storage.instances.values():
             proc.kill()
 
+def sigint_handle(sig, frame):
+    raise KeyboardInterrupt
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sigint_handle)
     try:
         main(parse_args())
     except KeyboardInterrupt:
-        logging.info('Got Ctrl+C, shutting down')
+        logging.info('Got Ctrl+C or SIGINT, shutting down')
     finally:
         shutdown()
