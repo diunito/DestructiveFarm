fe4da763ef2aefa79d7fd704219a74673ac0bee8
koraynilay <koray.fra@gmail.com>
2024-06-16 18:46:47 +0200

    Update start_server.sh to use  env variable

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# check if ../.env has been sourced or not
+if [ -z $SYSTEM_PROTOCOL ];then
+	echo -e "\n\n\ncustomize and source ../.env before starting this script to have a working configuration\n\n\n"
+fi
+
 # Use FLASK_DEBUG=True if needed
 
-FLASK_APP=$(dirname $(readlink -f $0))/standalone.py python3 -m flask run --host 0.0.0.0 --with-threads
+FLASK_APP="$(dirname "$(readlink -f "$0")")"/standalone.py python3 -m flask run --host 0.0.0.0 $(if [ -n "$FLASK_PORT" ];then echo --port "$FLASK_PORT"; fi) --with-threads
