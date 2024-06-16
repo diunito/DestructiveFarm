a8dd42d43d9c5540df674a150af8da2478837a41
koraynilay <koray.fra@gmail.com>
2024-06-16 18:31:30 +0200

    Update config.py and .env

--- server/config.py
+++ server/config.py
@@ -6,24 +6,25 @@ CONFIG = {
     # The clients will run sploits on TEAMS and
     # fetch FLAG_FORMAT from sploits' stdout.
     'TEAMS': {'Team #{}'.format(i): '10.60.{}.1'.format(i)
-              for i in range(1, 45 + 1)},
-    'FLAG_FORMAT': os.environ.get(r'FLAG_FORMAT', r'[A-Z0-9]{31}='),
+              for i in range(1, int(os.environ.get('TEAM_NUM', 45)) + 1)},
+    'FLAG_FORMAT': os.environ.get('FLAG_FORMAT', r'[A-Z0-9]{31}='),
 
     # This configures how and where to submit flags.
     # The protocol must be a module in protocols/ directory.
+    # (Default values are there just for reference)
 
     'SYSTEM_PROTOCOL': os.environ.get('SYSTEM_PROTOCOL', 'volgactf'),
+    # HOST, PORT, TOKEN for tcp
     'SYSTEM_HOST': os.environ.get('SYSTEM_HOST', '10.10.10.10'),
     'SYSTEM_PORT': int(os.environ.get('SYSTEM_PORT', 31337)),
+    'TEAM_TOKEN': os.environ.get('TEAM_TOKEN', 'your_secret_token'),
+    # URL, TOKEN for http
     'SYSTEM_URL': os.environ.get('SYSTEM_URL', 'http://monitor.ructfe.org/flags'),
     'SYSTEM_TOKEN': os.environ.get('SYSTEM_TOKEN', 'your_secret_token'),
     
-    
-    'TEAM_TOKEN': os.environ.get('TEAM_TOKEN', 'your_secret_token'),
-    
-    #'SYSTEM_PROTOCOL': 'ructf_tcp',
-    #'SYSTEM_HOST': '127.0.0.1',
-    #'SYSTEM_PORT': 31337,
+    # 'SYSTEM_PROTOCOL': 'ructf_tcp',
+    # 'SYSTEM_HOST': '127.0.0.1',
+    # 'SYSTEM_PORT': 31337,
 
     # 'SYSTEM_PROTOCOL': 'ructf_http',
     # 'SYSTEM_URL': 'http://monitor.ructfe.org/flags',
@@ -41,7 +42,7 @@ CONFIG = {
     # every SUBMIT_PERIOD seconds. Flags received more than
     # FLAG_LIFETIME seconds ago will be skipped.
     'SUBMIT_FLAG_LIMIT': float(os.environ.get('SUBMIT_FLAG_LIMIT', 100)),
-    'SUBMIT_PERIOD': float(os.environ.get('SUBMIT_PERIOD', 5)),    
+    'SUBMIT_PERIOD': float(os.environ.get('SUBMIT_PERIOD', 5)),
     'FLAG_LIFETIME': float(os.environ.get('FLAG_LIFETIME', 60)),
 
     # Password for the web interface. You can use it with any login.
@@ -49,6 +50,6 @@ CONFIG = {
     'SERVER_PASSWORD': os.environ.get('SERVER_PASSWORD', '1234'),
 
     # Use authorization for API requests
-    'ENABLE_API_AUTH': False,
-    'API_TOKEN': '00000000000000000000'
+    'ENABLE_API_AUTH': bool(os.environ.get('ENABLE_API_AUTH', "false").lower() == "true"), # bool() just to be sure
+    'API_TOKEN': os.environ.get('API_TOKEN', 'Tok3N')
 }
